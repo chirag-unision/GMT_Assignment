7ce98ec58738be78d29abbd7a2ecb8c923be07ef
chirag-unision <sharmacj123@gmail.com>
2024-04-11 23:49:46 +0530

    added express backend and some changes in app

--- src/constants/config.tsx
+++ src/constants/config.tsx
@@ -1,5 +1,5 @@
 export default {
-    BASE_URL: 'https://e720-2401-4900-1c68-e73e-b1f9-f7f0-9291-562e.ngrok-free.app/api/',
+    BASE_URL: 'https://gmtbackend-production.up.railway.app/api/',
     WEBCLIENT: '689329845646-p41j4fsloj8atc83dsvou65du2ju3fn4.apps.googleusercontent.com'
   };
   
